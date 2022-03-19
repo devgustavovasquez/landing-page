ff3aa04b832db5d7750787ec2a16f5d83d8ecc8d
Tafuso <gustavovasquez2002@gmail.com>
2022-03-19 10:54:37 -0300

    add storybook and config project

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,8 +1,14 @@
 export const theme = {
   colors: {
-    mainBg: 'pink',
-    secondaryBg: 'blue',
+    primaryColor: '#0a1128',
+    secondaryColor: '#dc143c',
+    white: '#ffffff',
   },
-  fonts: {},
+  fonts: {
+    family: {
+      default: "'Open Sans', sans-serif",
+      secondary: "'Montserrat', sans-serif",
+    },
+  },
   spaces: {},
 };
