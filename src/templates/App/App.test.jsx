b8e1074c8fd004c7580e20a94b94a442deaac5dd
Tafuso <gustavovasquez2002@gmail.com>
2022-03-19 12:46:26 -0300

    add new styles to project

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -1,15 +1,8 @@
-import { screen } from '@testing-library/react';
 import Home from '.';
 import { renderTheme } from '../../styles/render-theme';
-import { theme } from '../../styles/theme';
 
-test('renders learn react link', () => {
-  renderTheme(<Home />);
-  const headingContainer = screen.getByRole('heading', { name: 'hello' }).parentElement;
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.mainBg,
+describe('<Home />', () => {
+  test('should render home', () => {
+    renderTheme(<Home />);
   });
-
-  expect(headingContainer).toHaveStyleRule('background', 'pink');
-  expect(headingContainer).toMatchSnapshot();
 });
