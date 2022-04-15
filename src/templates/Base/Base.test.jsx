e8314c3f504476c7b53b4e257eed94be1a33ac5a
Tafuso <gustavovasquez2002@gmail.com>
2022-04-15 12:54:05 -0300

    test Base template

--- src/templates/Base/Base.test.jsx
+++ src/templates/Base/Base.test.jsx
@@ -1,10 +1,11 @@
 import { screen } from '@testing-library/react';
 import { Base } from '.';
 import { renderTheme } from '../../styles/render-theme';
+import { mockBase } from './mock';
 
 describe('<Base />', () => {
-  test('should render', () => {
-    renderTheme(<Base>text</Base>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  test('should match snapshot', () => {
+    const { container } = renderTheme(<Base {...mockBase} />);
+    expect(container).toMatchSnapshot();
   });
 });
