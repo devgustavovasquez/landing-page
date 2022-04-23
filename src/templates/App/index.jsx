94962603cfca856c075d81f8d6fa2595f8758bd0
Tafuso <gustavovasquez2002@gmail.com>
2022-04-23 00:21:40 -0300

    rename template App to Home and config them

--- src/templates/App/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Base } from '../Base';
-import { mockBase } from '../Base/mock';
-import * as Styled from './styles';
-
-const Home = () => {
-  return <Base {...mockBase} />;
-};
-
-export default Home;
---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+
+import * as Styled from './styles';
+
+import { mapData } from '../../api/map-data';
+
+import { GridTwoColumn } from '../../components/GridTwoColumn';
+import { GridContent } from '../../components/GridContent';
+import { GridText } from '../../components/GridText';
+import { GridImage } from '../../components/GridImage';
+
+import { mockBase } from '../Base/mock';
+import { Base } from '../Base';
+import { PageNotFound } from '../PageNotFound';
+import { Loading } from '../Loading';
+
+function Home() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const data = await fetch('http://localhost:1337/pages/?slug=landing-page');
+        const json = await data.json();
+        console.log(json);
+        const pageData = mapData(json);
+        setData(pageData[0]);
+      } catch (e) {
+        console.log(e);
+        setData(undefined);
+      }
+    };
+    load();
+  }, []);
+  if (data === undefined) {
+    return <PageNotFound />;
+  }
+  if (data && !data.slug) {
+    return <Loading />;
+  }
+  console.log(data);
+  const { menu, footerHtml, slug } = data;
+  const { links, text, link, srcImg } = menu;
+  const sections = data.sections;
+  return (
+    <Base links={links} footerHtml={footerHtml} logoData={{ text, link, srcImg }}>
+      {sections.map((section, index) => {
+        const { component } = section;
+        const key = `${slug}-${index}`;
+
+        if (component === 'section.section-two-columns') {
+          return <GridTwoColumn key={key} {...section} />;
+        }
+
+        if (component === 'section.section-content') {
+          return <GridContent key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-text') {
+          return <GridText key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-image') {
+          return <GridImage key={key} {...section} />;
+        }
+      })}
+    </Base>
+  );
+}
+
+export default Home;
